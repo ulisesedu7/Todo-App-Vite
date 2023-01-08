8aff1fccd0f1549dfcc592c1f6caa9254d602911
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2023-01-08 17:35:39 -0600

    Implement my todo app project with a vite project

--- js/main.js
+++ js/main.js
@@ -1,7 +1,61 @@
+// Imports de nuestro modulos
+import Items from "./modules/add-remove.js";
+import ToDoItem from "./modules/item-constructor.js";
+import LocalStorage from "./modules/local-storage.js";
+
+// Imports de los estilos
 import '../assets/styles/style.scss';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <p>Esta es mi plantilla para vanilla JS hecha con Vite</p>
-  </div>
-`;
+
+// Constantes iniciales
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+
+const inputDescription = document.getElementById('todo-item');
+
+const itemsContainer = document.getElementById('todo-cont');
+
+// Llamar data si existe en cuando cargue la pagina
+Items.displayItems();
+
+// Evento de escucha de la forma
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
+
+// Evento de escucha main para ingresar elementos
+main.addEventListener('keypress', (e) => {
+  // Obtener los valores
+  const description = inputDescription.value;
+
+  // Validacion
+  if(description == '') {
+     // Vacia por ahora para luego llamar la funcion validadora
+  }
+
+  // Mandar los valores
+  if(e.key === 'Enter' && description != '') {
+    const item = new ToDoItem(false, description);
+
+    // Agrear un nuevo todo
+    Items.addItem(item);
+
+    // Agregarlo al Local Storage
+    LocalStorage.addItem(item);
+
+    // Limpiar la input
+    Items.clearInput();
+  }
+});
+
+// Evento de escucha para remover elementos
+itemsContainer.addEventListener('click', (e) => {
+  // Remover Elemento
+  if(e.target.classList.contains('remove')){
+    Items.removeItem(e.target);
+
+    LocalStorage.removeItem(
+      e.target.parentElement.previousElementSibling.lastElementChild.textContent,
+    );
+  }
+});
